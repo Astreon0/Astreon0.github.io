ba202f4783cd47a6c5b398c5a3bc4017845e5269
Romain <romain.lejman@gmail.com>
2022-05-17 14:32:59 +0200

    Add red&orange color on 10&5

--- WarTimer.js
+++ WarTimer.js
@@ -1,6 +1,6 @@
 let secondeTotal = 1809;
-
-let secondesTimerTab = [9, 
+let secondesTimerTab = [];
+let initialTimerTab = [9,                                   
     20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, //14 = 280 sec
     28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28,         //12 = 336 sec
     36, 36, 36, 36, 36, 36, 36, 36, 36,                     // 9 = 324 sec
@@ -9,18 +9,28 @@ let secondesTimerTab = [9,
     60, 60, 60, 60, 60];                                    // 5 = 300 sec
 
 let index = 0;
+let alreadyOn = false;
+let color10 = "red";
+let color5 = "orange";
+let color0 = "green";
+let basicColor = "black";
 
 function startTimer() {
-    const preTimerElement = document.getElementById("preTimer");
-    const timerElement = document.getElementById("timer");
-    const timerTotalElement = document.getElementById("timerTotal");
-
-    preTimerElement.style.display = "none";
-    timerElement.style.display = "block";
-
-    setInterval(() => {
-        diminuerSeconde();
-    }, 1000);
+    if (!alreadyOn) {
+        alreadyOn = true;
+        document.getElementById("startTimer").style.display = "none";
+        secondesTimerTab = initialTimerTab;
+        const preTimerElement = document.getElementById("preTimer");
+        const timerElement = document.getElementById("timer");
+        const timerTotalElement = document.getElementById("timerTotal");
+    
+        preTimerElement.style.display = "none";
+        timerElement.style.display = "block";
+    
+        setInterval(() => {
+            diminuerSeconde();
+        }, 1000);
+    }
 }
 
 function diminuerSeconde() {
@@ -46,6 +56,8 @@ function diminuerSeconde() {
 
     secondeTotal = secondeTotal <= 0 ? 0 : secondeTotal - 1;
     secondesTimerTab[index] = secondesTimerTab[index] <= 0 ? 0 : secondesTimerTab[index] - 1;
+
+    timerElement.style.color = secondesTimerTab[index] < 10 ? (secondesTimerTab[index] < 5 ? color5 : color10) : basicColor;
 
     if(secondesTimerTab[index] == 0) {
         index++;
